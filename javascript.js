6f5a36ce538f03c4cfdadc0b3ef44f44ff3aa459
feveny <yfeven8@gmail.com>
2024-01-20 16:32:53 +0300

    Lets eat dinner

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const wrapper = document.getElementById("wrapper");
+const question = document.getElementById("question");
+const gif = document.getElementById("gif");
+const yesBtn = document.getElementById("yes-btn");
+const noBtn = document.getElementById("no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yay, see you in the kitchen!";
+  gif.src = "https://i.pinimg.com/originals/ee/f6/a4/eef6a46e97fc7428ad471c2e02bdb2da.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
